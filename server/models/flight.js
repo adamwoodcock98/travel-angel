13bac10a1254d6adade07f64dc2a3dc231721d74
Adam Woodcock <42064260+adamwoodcock98@users.noreply.github.com>
2022-05-04 15:10:48 +0100

    Created flight schema

--- server/models/flight.js
+++ server/models/flight.js
@@ -1,9 +1,36 @@
 const mongoose = require("mongoose");
 
 const flightSchema = new mongoose.Schema({
-  flightNumber: {
+  flightNumber: String,
+  departureTime: {
+    type: Date,
+    required: true,
+  },
+  airline: String,
+  departureAirport: {
     type: String,
     required: true,
+  },
+  departureCity: {
+    type: String,
+    required: true,
+  },
+  departureTerminal: String,
+  departureGate: String,
+  arrivalAirport: {
+    type: String,
+    required: true,
+  },
+  arrivalCity: {
+    type: String,
+    required: true,
+  },
+  arrivalTerminal: String,
+  arrivalGate: String,
+  bookingReference: String,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
   }
 })
 
