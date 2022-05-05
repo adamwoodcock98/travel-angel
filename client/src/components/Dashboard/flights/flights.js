28a103907d83a6df4e569d16f6d07b6242fbac80
Adam Woodcock <42064260+adamwoodcock98@users.noreply.github.com>
2022-05-05 17:20:27 +0100

    Setup Flights page
    Rendered multiple flight cards onto flight page
    Style the layout only
    Setup CSS for flights page

--- client/src/components/Dashboard/flights/flights.js
+++ client/src/components/Dashboard/flights/flights.js
@@ -23,11 +23,26 @@ const Flights = () => {
     });
   }, [])
 
-  if (outboundFlight.length) {
+  if (outboundFlight.length || inboundFlight.length) {
     return(
-      <>
-        <FlightCard outboundFlight={outboundFlight[0]} />
-      </>
+      <div className="flights-window">
+        <div className="flights-header">
+          <h1>Your flights</h1>
+        </div>
+        <div className="flights-content">
+          <div className="flights-content-outbound">
+            <h1 className="flights-content-subheading">Outbound</h1>
+            {outboundFlight[0] && <FlightCard outboundFlight={outboundFlight[0]} />}
+          </div>
+          <div className="flights-content-inbound">
+            <h1 className="flights-content-subheading">Inbound</h1>
+            {inboundFlight[0] && <FlightCard outboundFlight={inboundFlight[0]} />}
+          </div>
+        </div>
+        <div className="flights-footer">
+
+        </div>
+      </div>
     )
   } else {
     return(
