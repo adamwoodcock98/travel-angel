97c303c1ebee2e30066f1d444cfcf9f536f1e5ca
Adam Woodcock <42064260+adamwoodcock98@users.noreply.github.com>
2022-05-06 18:12:12 +0100

    Populated parking schema

--- server/models/parking.js
+++ server/models/parking.js
@@ -1,7 +1,27 @@
 const mongoose = require("mongoose");
 
 const parkingSchema = new mongoose.Schema({
-  
+  startDate: {
+    type: Date,
+    required: true,
+  },
+  endDate: {
+    type: Date,
+    required: true,
+  },
+  address: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Address",
+  },
+  regPlate: String,
+  company: String,
+  contactNumber: Number,
+  bookingRefernce: String,
+  notes: String,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
 });
 
 const Parking = mongoose.model("Parking", parkingSchema);
