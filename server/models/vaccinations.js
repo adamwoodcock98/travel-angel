4274f6ca17dd2f85fc17d1a709c4b6f55779e447
Adam Woodcock <42064260+adamwoodcock98@users.noreply.github.com>
2022-05-07 16:04:17 +0100

    Created vaccinations schema

--- server/models/vaccinations.js
+++ server/models/vaccinations.js
@@ -1,7 +1,22 @@
 const mongoose = "mongoose";
 
 const vaccinationsSchema = new mongoose.schema({
-
+  vaccinationStatus: {
+    type: String,
+    default: "Unvaccinated",
+  },
+  vaccineProof: [Buffer],
+  additionalDocuments: [Buffer],
+  vaccineDoses: [{
+    dose: String,
+    date: Date,
+    type: String,
+  }],
+  user: {
+    type: String,
+    ref: "User",
+    required: true,
+  }
 });
 
 const Vaccinations = mongoose.model("Vaccinations", vaccinationsSchema);
