0b0073ef01bcc27b37453ca2658fd93bedb538f8
ruiined <ruiined@gmail.com>
2022-05-07 17:22:40 +0100

    Added a signature

--- client/src/components/dashboard/passport/displayPassport.js
+++ client/src/components/dashboard/passport/displayPassport.js
@@ -23,44 +23,49 @@ export const DisplayPassport = ({
           <div className="pass-card">
             <div className="pass-country">{passport.country}</div>
             <div className="pass-pic">
+              <div className="pass-passport">Passport<br />Passeport</div>
               <img alt="Passport" src="http://localhost:3000/pass-pic.gif" />
             </div>
-            <div classNmae="pass-contents">
+            <div className="pass-contents">
+              <div className="pass-type"><h5>Type/Type</h5>&nbsp;P</div>
               <div className="pass-num">
                 <h5>Passport No./Passeport No.</h5>
                 {passport.passportNumber}
               </div>
               <div className="pass-last-name">
-                <h5>Surname/Nom</h5>
-                {passport.lastName}
+                <h5>Surname/Nom (1)</h5>
+                &nbsp;{passport.lastName}
               </div>
               <div className="pass-first-name">
-                <h5>Given names/Prénoms</h5>
-                {passport.firstName}
+                <h5>Given names/Prénoms (2)</h5>
+                &nbsp;{passport.firstName}
               </div>
               <div className="pass-nationality">
-                <h5>Nationality/Nationalité</h5>
-                {passport.nationality}
+                <h5>Nationality/Nationalité (3)</h5>
+                &nbsp;{passport.nationality}
               </div>
               <div className="pass-dob">
-                <h5>Date of birth/Date de naissance</h5>
-                {formatPassDate(passport.dob)}
+                <h5>Date of birth/Date de naissance (4)</h5>
+                &nbsp;{formatPassDate(passport.dob)}
               </div>
               <div className="pass-gender">
-                <h5>Sex/Sexe</h5>
-                {passport.gender}
+                <h5>Sex/Sexe (5)</h5>
+                &nbsp;{passport.gender}
               </div>
               <div className="pass-birth-place">
-                <h5>Place of birth/Lieu de naissance</h5>
-                {passport.placeOfBirth}
+                <h5>Place of birth/Lieu de naissance (6)</h5>
+                &nbsp;{passport.placeOfBirth}
               </div>
               <div className="pass-issue">
-                <h5>Date of issue/Date d'émission</h5>
-                {formatPassDate(passport.dateOfIssue)}
+                <h5>Date of issue/Date d'émission (7)</h5>
+                &nbsp;{formatPassDate(passport.dateOfIssue)}
               </div>
               <div className="pass-expiry">
-                <h5>Date d'expiration</h5>
-                {formatPassDate(passport.dateOfExpiry)}
+                <h5>Date d'expiration (8)</h5>
+                &nbsp;{formatPassDate(passport.dateOfExpiry)}
+              </div>
+              <div className="pass-signature">
+                {passport.firstName} &nbsp;{passport.lastName}
               </div>
             </div>
           </div>
