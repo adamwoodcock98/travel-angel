38715707fe13e5da9c0b758615c9b6d793f02cc3
Kathleen Ly <heykathl@users.noreply.github.com>
2022-05-07 13:08:21 +0100

    Created 'new' controller

--- server/controllers/visas.js
+++ server/controllers/visas.js
@@ -1,9 +1,28 @@
-const Visa = require("..models/visas.js");
+const Visa = require("../models/visa.js")
 
-const VisaController = {
+const VisasController = {
   Index: (res, req) => {},
 
-  New: (req, res) => {},
+  New: async (req, res) => {
+    const data = req.body;
+
+    try {
+      const visa = new Visa({
+        visaNumber: data.visaNumber,
+        startDate: data.startDate,
+        endDate: data.endDate,
+        issuingCountry: data.issuingCountry,
+        // user:
+      });
+
+      await visa.save();
+
+      res.status(200).send();
+    } catch(e){
+        console.log(e.message)
+        res.status(500).send(e)
+    } 
+  },
 };
 
-module.exports = VisaController;
+module.exports = VisasController;
--- server/routes/visas.js
+++ server/routes/visas.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const VisaController = require("../controllers/visas.js");
+const VisasController = require("../controllers/visas.js");
 
 const VisaRouter = express.Router();
-VisaRouter.get("/", VisaController.Index);
-VisaRouter.post("/", VisaController.Create);
+VisaRouter.get("/", VisasController.Index);
+VisaRouter.post("/", VisasController.Create);
 
 module.exports = VisaRouter;
