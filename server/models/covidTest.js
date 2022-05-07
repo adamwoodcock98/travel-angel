6358645beacddc80858ec3826eabb7d9e3f54e78
Adam Woodcock <42064260+adamwoodcock98@users.noreply.github.com>
2022-05-07 16:56:07 +0100

    Test drove covid test schema

--- server/models/covidTest.js
+++ server/models/covidTest.js
@@ -1,7 +1,22 @@
 const mongoose = require("mongoose");
 
 const covidTestSchema = mongoose.Schema({
-
+  testType: {
+    type: String,
+    required: true,
+  },
+  isReminder: {
+    type: Boolean,
+    required: true,
+  },
+  result: String,
+  testDate: Date,
+  testFromDate: Date,
+  resultByDate: Date,
+  validToDate: Date,
+  testNumber: String,
+  testCountry: String,
+  testProvider: String,
 });
 
 const CovidTest = mongoose.model("CovidTest", covidTestSchema);
--- server/spec/models/covidTest.test.js
+++ server/spec/models/covidTest.test.js
@@ -12,4 +12,136 @@ describe("Covid test model", () => {
     });
   });
 
+    advanceTo();
+    const mockTestDate = new Date();
+    const mockFromDate = new Date();
+    const mockByDate = new Date();
+    const mockToDate = new Date();
+
+  it("Stores a test type", () => {
+    const test = new CovidTest({
+      testType: "PCR",
+      isReminder: false
+    });
+
+    expect(test.testType).toEqual("PCR");
+  });
+
+  it("Stores a test type as a required field", async () => {
+    const test = new CovidTest({
+      isReminder: false
+    });
+
+    await expect(test.save()).rejects.toThrow();
+  });
+
+  it("Stores if its a reminder or results", () => {
+    const test = new CovidTest({
+      testType: "PCR",
+      isReminder: false
+    });
+
+    expect(test.isReminder).toEqual(false);
+  });
+
+  it("Stores if its a reminder or results as a required field", async () => {
+    const test = new CovidTest({
+      testType: "PCR",
+    });
+
+    await expect(test.save()).rejects.toThrow();
+  });
+
+  it("Stores a result", () => {
+    const test = new CovidTest({
+      testType: "PCR",
+      isReminder: false,
+      result: "Positive",
+    });
+
+    expect(test.result).toEqual("Positive");
+  });
+
+  it("Stores a testDate", () => {
+    advanceTo();
+    const mockDate = new Date();
+
+    const test = new CovidTest({
+      testType: "PCR",
+      isReminder: false,
+      testDate: mockDate,
+    });
+
+    expect(test.testDate).toEqual(mockDate);
+  });
+
+  it("Stores a test from date", () => {
+    advanceTo();
+    const mockDate = new Date();
+
+    const test = new CovidTest({
+      testType: "PCR",
+      isReminder: false,
+      testFromDate: mockDate,
+    });
+
+    expect(test.testFromDate).toEqual(mockDate);
+  });
+
+  it("Stores a date the user must have results by", () => {
+    advanceTo();
+    const mockDate = new Date();
+
+    const test = new CovidTest({
+      testType: "PCR",
+      isReminder: false,
+      resultByDate: mockDate,
+    });
+
+    expect(test.resultByDate).toEqual(mockDate);
+  });
+
+  it("Stores a date the result is valid until", () => {
+    advanceTo();
+    const mockDate = new Date();
+
+    const test = new CovidTest({
+      testType: "PCR",
+      isReminder: false,
+      validToDate: mockDate,
+    });
+
+    expect(test.validToDate).toEqual(mockDate);
+  });
+
+  it("Stores a test numbber", () => {
+    const test = new CovidTest({
+      testType: "PCR",
+      isReminder: false,
+      testNumber: "MGG06350726",
+    });
+
+    expect(test.testNumber).toEqual("MGG06350726");
+  });
+
+  it("Stores a testing country", () => {
+    const test = new CovidTest({
+      testType: "PCR",
+      isReminder: false,
+      testCountry: "UK",
+    });
+
+    expect(test.testCountry).toEqual("UK");
+  });
+
+  it("Stores a test provider", () => {
+    const test = new CovidTest({
+      testType: "PCR",
+      isReminder: false,
+      testProvider: "NHS",
+    });
+
+    expect(test.testProvider).toEqual("NHS");
+  });
+
 });
