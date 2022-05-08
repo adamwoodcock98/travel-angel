e1ccd26ef4389d76bfe165ddcd70d677b6e20d27
Adam Woodcock <42064260+adamwoodcock98@users.noreply.github.com>
2022-05-08 12:06:11 +0100

    Structured test card component

---
+++ client/src/components/Dashboard/covid/tests/testCard.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export const TestCard = (props) => {
+  const testData = props.testData;
+
+  return(
+    <div className="test-card">
+      <div className="test-card-header">
+
+      </div>  
+      <div className="test-card-primary-content">
+        <div className="test-card-primary-left">
+
+        </div>
+        <div className="test-card-primary-right">
+
+        </div>
+      </div>
+      <div className="test-card-secondary-content">
+        <div className="test-card-secondary-left">
+          
+        </div>
+        <div className="test-card-secondary-right">
+
+        </div>
+      </div>
+      <div className="test-card-tertiary-content">
+        <div className="test-card-tertiary-left">
+          
+        </div>
+        <div className="test-card-tertiary-right">
+
+        </div>
+      </div>
+    </div>
+  )
+}
