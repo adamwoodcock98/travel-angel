3044aff1380d3d3f94a324c773cce04caa952e37
Adam Woodcock <42064260+adamwoodcock98@users.noreply.github.com>
2022-05-09 13:55:01 +0100

    Deleted unwanted files

--- server/routes/visas 2.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const VisaController = require("../controllers/visas.js");
-
-const VisaRouter = express.Router();
-VisaRouter.get("/", VisaController.Index);
-VisaRouter.post("/", VisaController.Create);
-
-module.exports = VisaRouter;
