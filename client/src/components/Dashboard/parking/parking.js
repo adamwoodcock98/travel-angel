b2094d944ab9d1b071f06cfbc0e85cd5ef434f64
Adam Woodcock <42064260+adamwoodcock98@users.noreply.github.com>
2022-05-09 09:22:55 +0100

    Created crud component for parking

--- client/src/components/Dashboard/parking/parking.js
+++ client/src/components/Dashboard/parking/parking.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import AddParking from "./addParking";
+import AddParking from "./addParking/addParking";
 import ParkingCard from "./viewParking";
 
 const Parking = ({ session }) => {
