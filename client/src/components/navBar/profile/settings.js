6c7263b625771db009bbff389cd09504d1fe79d5
ruiined <ruiined@gmail.com>
2022-05-10 00:49:36 +0100

    Deleted some tests

--- client/src/components/navBar/profile/settings.js
+++ client/src/components/navBar/profile/settings.js
@@ -1,13 +1,19 @@
 import React, { useState, useEffect } from "react";
-import { Dialog, Button, TextField, DialogTitle, DialogActions, DialogContent, DialogContentText } from "@mui/material";
+import {
+  Dialog,
+  Button,
+  TextField,
+  DialogTitle,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+} from "@mui/material";
 import axios from "axios";
 import { Alerts } from "../../assets/snackbar";
-import { Profile } from "./profile"
+import { Profile } from "./profile";
 
-export const Settings = ({
-  session,
-}) => {
-  const userId = session
+export const Settings = ({ session }) => {
+  const userId = session;
   const [open, setOpen] = useState(false);
   const [user, setUser] = useState("");
 
@@ -22,7 +28,6 @@ export const Settings = ({
   const handleAlertClose = () => {
     setAlertOpen(false);
   };
-
 
   const handleOpen = () => {
     setOpen(true);
@@ -44,13 +49,21 @@ export const Settings = ({
     e.preventDefault();
 
     const { firstName, lastName, email, password, profilePicture } = user;
-    const updatedUser = { firstName, lastName, email, password, profilePicture };
+    const updatedUser = {
+      firstName,
+      lastName,
+      email,
+      password,
+      profilePicture,
+    };
 
-    await axios.post(`http://localhost:8000/user/${userId}/save`, updatedUser).then((res) => {
-      handleClose();
-      <Profile session={res.data.user._id} />  
-      handleAlert(res.data.msg, res.data.type);
-    });
+    await axios
+      .post(`http://localhost:8000/user/${userId}/save`, updatedUser)
+      .then((res) => {
+        handleClose();
+        <Profile session={res.data.user._id} />;
+        handleAlert(res.data.msg, res.data.type);
+      });
   };
 
   useEffect(() => {
@@ -69,94 +82,79 @@ export const Settings = ({
 
   if (user) {
     return (
-    <div>
-      <div onClick={handleOpen}>
-       Settings
+      <div>
+        <div onClick={handleOpen}>Settings</div>
+        <Dialog open={open} onClose={handleClose}>
+          <DialogTitle>Settings</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              <TextField
+                value={user.firstName}
+                autoFocus
+                fullWidth
+                margin="dense"
+                id="firstName"
+                name="firstName"
+                label="First Name"
+                type="text"
+                variant="outlined"
+                required
+                onChange={handleChange}
+              />
+              <TextField
+                value={user.lastName}
+                autoFocus
+                fullWidth
+                margin="dense"
+                id="lastName"
+                name="lastName"
+                label="Last Name"
+                type="text"
+                variant="outlined"
+                required
+                onChange={handleChange}
+              />
+              <TextField
+                value={user.email}
+                autoFocus
+                fullWidth
+                margin="dense"
+                id="email"
+                name="email"
+                label="E-mail"
+                type="email"
+                variant="outlined"
+                required
+                onChange={handleChange}
+              />
+              <TextField
+                value={user.profilePicture}
+                autoFocus
+                margin="dense"
+                id="profilePicture"
+                name="profilePicture"
+                label="Profile Picture"
+                type="text"
+                variant="outlined"
+                onChange={handleChange}
+              />
+            </DialogContentText>
+            <DialogActions>
+              <Button onClick={handleClose}>Cancel</Button>
+              <Button onClick={handleSubmit}>Save</Button>
+            </DialogActions>
+          </DialogContent>
+        </Dialog>
+        <Alerts
+          message={alertMessage}
+          open={alertOpen}
+          handleClose={handleAlertClose}
+          alertPosition={alertPosition}
+          alertType={alertType}
+        />
       </div>
-      <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>Settings</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            <TextField
-              value={user.firstName}
-              autoFocus
-              fullWidth
-              margin="dense"
-              id="firstName"
-              name="firstName"
-              label="First Name"
-              type="text"
-              variant="outlined"
-              required
-              onChange={handleChange}
-            />
-            <TextField
-              value={user.lastName}
-              autoFocus
-              fullWidth
-              margin="dense"
-              id="lastName"
-              name="lastName"
-              label="Last Name"
-              type="text"
-              variant="outlined"
-              required
-              onChange={handleChange}
-            />
-            <TextField
-              value={user.email}
-              autoFocus
-              fullWidth
-              margin="dense"
-              id="email"
-              name="email"
-              label="E-mail"
-              type="email"
-              variant="outlined"
-              required
-              onChange={handleChange}
-            />
-            {/* <TextField
-              value={user.password}
-              autoFocus
-              fullWidth
-              margin="dense"
-              id="password"
-              name="password"
-              label="Password"
-              type="password"
-              variant="outlined"
-              required
-              onChange={handleChange}
-            /> */}
-            <TextField
-              value={user.profilePicture}
-              autoFocus
-              margin="dense"
-              id="profilePicture"
-              name="profilePicture"
-              label="Profile Picture"
-              type="text"
-              variant="outlined"
-              onChange={handleChange}
-            />
-          </DialogContentText>
-          <DialogActions>
-            <Button onClick={handleClose}>Cancel</Button>
-            <Button onClick={handleSubmit}>Save</Button>
-          </DialogActions>
-        </DialogContent>
-      </Dialog>
-      <Alerts
-        message={alertMessage}
-        open={alertOpen}
-        handleClose={handleAlertClose}
-        alertPosition={alertPosition}
-        alertType={alertType}
-      />
-    </div>
     );
   } else {
-    return <i>...</i>
+    return <i>...</i>;
   }
 };
