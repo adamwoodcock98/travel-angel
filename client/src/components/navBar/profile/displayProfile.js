b4627043b51e12ca5d6ab67a1181f42b2b64ba1e
ruiined <ruiined@gmail.com>
2022-05-10 11:49:26 +0100

    Updated profile display

--- client/src/components/navBar/profile/displayProfile.js
+++ client/src/components/navBar/profile/displayProfile.js
@@ -1,17 +1,17 @@
-import React, { useState } from "react";
-import { DialogTitle, DialogContent, DialogContentText } from "@mui/material";
+import React from "react";
+import { Dialog, DialogContentText } from "@mui/material";
 import moment from "moment";
 
 export const DisplayProfile = ({ user }) => {
   return (
     <div>
-      <DialogTitle>{user.firstName} {user.lastName}</DialogTitle>
-      <DialogContent>
+        <DialogContentText>
         <img alt="Profile" src={user.profilePicture} />
-          <DialogContentText>
-            E-mail: {user.email}
-          </DialogContentText>
-      </DialogContent>
+        </DialogContentText>
+        <DialogContentText id="profile-txt">
+        <h3>{user.firstName} {user.lastName}</h3>
+          <i>{user.email}</i>
+        </DialogContentText>
     </div>
   );
 };
