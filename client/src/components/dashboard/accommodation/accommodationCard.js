cf547a457d5d081eaf9fde970d275e4dc652d752
Kathleen Ly <heykathl@users.noreply.github.com>
2022-05-12 18:02:01 +0100

    Accommodation card styling

--- client/src/components/dashboard/accommodation/accommodationCard.js
+++ client/src/components/dashboard/accommodation/accommodationCard.js
@@ -56,25 +56,24 @@ export default function AccommodationCard(props) {
                   </div>
                   <div className="check-in-body">
                     <h3>{formatDate(accommodation.checkInDate)}</h3>
-                    <p>Check-in opens at {accommodation.checkInTime}</p>
+                    <p>Check-in opens at</p><p className="check-out-time">{accommodation.checkInTime}</p>
                   </div>
                 </div>
                 <div className="subbody-right">
                   <div className="check-out-header">
-                
-                      <p>Check-Out</p>
-              
+                    <p>Check-Out</p>
                   </div>
                   <div className="check-out-body">
                     <h3>{formatDate(accommodation.checkOutDate)}</h3>
-                    <p>Check-out by {accommodation.checkOutTime}</p>
+                    <p>Check-out by</p><p className="check-out-time">{accommodation.checkOutTime}</p>
                   </div>
                 </div>
               </div>
               <div className="footer">
                 <div className="booking-reference">
+                  <p className="light-label">Booking Reference:</p>
                   {accommodation.bookingReference && (
-                    <p>Booking Reference: {accommodation.bookingReference}</p>
+                    <h4>{accommodation.bookingReference}</h4>
                   )}
                 </div>
                 <div className="contact-number">
@@ -83,13 +82,15 @@ export default function AccommodationCard(props) {
                   )}
                 </div>
                 <div className="address">
+                  <p className="light-label">Address: </p>
                   {accommodation.address && (
-                    <p>Address: {formatAddress(accommodation.address)}</p>
+                    <h4>{formatAddress(accommodation.address)}</h4>
                   )}
                 </div>
                 <div className="directions">
                   <Button
-                    color="secondary"
+                    color="primary"
+                    variant="outlined"
                     startIcon={<DirectionsOutlinedIcon />}
                     target="_blank"
                     href={handleDirections(accommodation.address)}
